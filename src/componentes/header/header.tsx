276a4713e4c177aa5c5aa60a0a2c37e35cc608d3
desplora <desploraviajes@gmail.com>
2024-03-28 20:59:33 +0100

    diego

--- src/componentes/header/header.tsx
+++ src/componentes/header/header.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 //estilos
 import headStyle from './header.module.css';
 
 //extenciones
-import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 //icons
 import { FaHome } from "react-icons/fa"; //casa
@@ -11,24 +13,30 @@ import { TiThMenu } from "react-icons/ti"; //menu hamburguesa
 import { GiPretzel } from "react-icons/gi"; // Brezel
 import { BiWorld } from "react-icons/bi"; // mundo
 import { IoHeart } from "react-icons/io5"; // corazon
+import { IoMdSunny } from "react-icons/io"; // sol
+import { FaMoon } from "react-icons/fa"; // luna
 
+let dark = true
 
 
 
 export default function Header() {
 
-        const handleMenuClik = ()=>{
+    const [navOpen, setNavOpen] = useState(false);
 
+    const movilNavHandler = () => {
+        const navMobilID = document.getElementById('navMobilID');
+        if (!navOpen) {
+            navMobilID.style.transform = 'translateY(0px)';
+        } else {
+            navMobilID.style.transform = 'translateY(-500px)';
         }
-
-
-
-
-
+        setNavOpen(!navOpen);
+    };
 
     return (
         <header className={headStyle.header}>
-            <div className={headStyle.menuDesktop}>
+            <div className={`${headStyle.menuDesktop} fondoAzulOscuro`}>
 
                 {/*Logo de desplora*/}
                 <Link className={headStyle.logo} href="/">
@@ -38,7 +46,7 @@ export default function Header() {
                 </Link>
 
                 {/*Navegacion con las paginas en el Desktop*/}
-                <nav className={headStyle.menuDesktopNav}>
+                <nav className={`${headStyle.menuDesktopNav} colorGrisClaro`}>
                     <ul>
                         <li><Link href="#"><FaHome className={headStyle.icon} /> Home</Link></li>
                         <li><Link href="#"><BiWorld className={headStyle.icon} /> Destinos</Link></li>
@@ -49,13 +57,18 @@ export default function Header() {
 
                 {/*Navegacion con las paginas en el movil*/}
 
-                <div className={headStyle.menuMobilIcon}>
-                <TiThMenu  size={40} /> 
+                <div className={`${headStyle.menuMobilIcon} colorGrisClaro`}>
+                <TiThMenu onClick={movilNavHandler}  size={40} /> 
                     </div>
 
                 {/*boton para light- y darkmode*/}
                 <div className={headStyle.themeSwitcher}>
                     <div className={headStyle.toggle}>
+
+                        {!dark?
+                    <IoMdSunny className={`${headStyle.toggleIcon} colorAmarillo`} /> :
+                    <FaMoon className={`${headStyle.toggleIcon} colorGrisClaro`}/>}
+
                     </div>
                 </div>
 
@@ -63,7 +76,7 @@ export default function Header() {
 
             {/*Menu desplegable para dispositivos moviles*/}
 
-            <div className={headStyle.menuMobil}>
+            <div id='navMobilID' className={headStyle.menuMobil}>
 
             <nav className={headStyle.menuMovilNav}>
                     <ul>
