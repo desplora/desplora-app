58daa3c36a8952a93d997cfbc551521ec96cb16e
desplora <desploraviajes@gmail.com>
2024-04-02 21:20:18 +0200

    bugs

--- src/app/destinos/[ciudad]/page.tsx
+++ src/app/destinos/[ciudad]/page.tsx
@@ -20,6 +20,18 @@ export default function Page({ params} : any) {
             <DestinoPortada tituloCiudad={dame.titulo} imagen={dame.imagenPrincipal} region={dame.region} geozona={dame.GeoZona} />
             <GaleriaFotos contenidoPayload={dame.galeria} />
             <TextoPrincipal contenidoPayload={dame.texto} />
+            <br/>
+            <h3>Alojamiento</h3>
+            <TextoPrincipal contenidoPayload={dame.Alojamiento} />
+            <br/>
+            <h3>Recomendaciones</h3>
+            <TextoPrincipal contenidoPayload={dame.recomendaciones} />
+            <br/>
+            <h3>Cosas para hacer</h3>
+            <br/>
+            <br/>
+            <p>publicado con amor el 04.03.2024</p>
+
             </>
              
              
@@ -27,7 +39,7 @@ export default function Page({ params} : any) {
              
              
              
-             : <h1>no hay datos</h1>}
+             : <p>loading...</p>}
         </div>
     )
 }
--- src/componentes/header/header.tsx
+++ src/componentes/header/header.tsx
@@ -69,9 +69,9 @@ export default function Header() {
                 <nav className={`${headStyle.menuDesktopNav} colorGrisClaro`}>
                     <ul>
                         <li><Link href="/"><FaHome className={headStyle.icon} /> Home</Link></li>
-                        <li><Link href="destinos"><BiWorld className={headStyle.icon} /> Destinos</Link></li>
-                        <li><Link href="alemania"><GiPretzel className={headStyle.icon} /> Vida en Alemania</Link></li>
-                        <li><Link href="nosotros"><IoHeart className={headStyle.icon} /> Nosotros</Link></li>
+                        <li><Link href="/destinos"><BiWorld className={headStyle.icon} /> Destinos</Link></li>
+                        <li><Link href="/alemania"><GiPretzel className={headStyle.icon} /> Vida en Alemania</Link></li>
+                        <li><Link href="/nosotros"><IoHeart className={headStyle.icon} /> Nosotros</Link></li>
                     </ul>
                 </nav>
 
@@ -111,9 +111,9 @@ export default function Header() {
             <nav className={headStyle.menuMovilNav}>
                     <ul>
                         <li><Link onClick={movilNavHandler} href="/"><FaHome className={headStyle.icon} /> Home</Link></li>
-                        <li><Link onClick={movilNavHandler} href="destinos"><BiWorld className={headStyle.icon} /> Destinos</Link></li>
-                        <li><Link onClick={movilNavHandler} href="alemania"><GiPretzel className={headStyle.icon} /> Vida en Alemania</Link></li>
-                        <li><Link onClick={movilNavHandler} href="nosotros"><IoHeart className={headStyle.icon} /> Nosotros</Link></li>
+                        <li><Link onClick={movilNavHandler} href="/destinos"><BiWorld className={headStyle.icon} /> Destinos</Link></li>
+                        <li><Link onClick={movilNavHandler} href="/alemania"><GiPretzel className={headStyle.icon} /> Vida en Alemania</Link></li>
+                        <li><Link onClick={movilNavHandler} href="/nosotros"><IoHeart className={headStyle.icon} /> Nosotros</Link></li>
                     </ul>
                 </nav>
 
